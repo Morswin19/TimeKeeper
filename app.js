56a36e1e4b08ae03dbf24d0fdb81d9a679ba5b97
Piotr Kalman <piotr.kalman85@gmail.com>
2021-02-23 21:54:18 +0100

    add html elements some styles

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-class Timer {}
+const time = document.querySelector('.time');
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+const reset = document.querySelector('.reset');
