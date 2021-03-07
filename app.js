80f38d00f91d3ac7804c494632e3300965ef4742
Piotr Kalman <piotr.kalman85@gmail.com>
2021-03-07 21:51:24 +0100

    add start function and add function

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const time = document.querySelector('.time');
-const start = document.querySelector('.start');
-const stop = document.querySelector('.stop');
-const reset = document.querySelector('.reset');
+const timeElement = document.querySelector('.time');
+const startButton = document.querySelector('.start');
+const stopButton = document.querySelector('.stop');
+const resetButton = document.querySelector('.reset');
+
+let time = 0;
+
+const startTime = () => {
+  setInterval(addTime, 100);
+};
+
+const addTime = () => {
+  time += 1;
+  console.log(time);
+};
+
+startButton.addEventListener('click', startTime);
