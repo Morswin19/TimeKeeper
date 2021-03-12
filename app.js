51aa111f2a3530b21a84ce58d6fae24b6ba8b9d3
Piotr Kalman <piotr.kalman85@gmail.com>
2021-03-12 21:51:47 +0100

    add timer functionality to the board

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const timeElement = document.querySelector('.time');
+const hoursElement = document.querySelector('.hours');
+const minutesElement = document.querySelector('.minutes');
+const secondsElement = document.querySelector('.seconds');
+const milisecondsElement = document.querySelector('.miliseconds');
 const startButton = document.querySelector('.start');
 const stopButton = document.querySelector('.stop');
 const resetButton = document.querySelector('.reset');
@@ -6,12 +10,13 @@ const resetButton = document.querySelector('.reset');
 let time = 0;
 
 const startTime = () => {
-  setInterval(addTime, 100);
+  setInterval(addTime, 10);
 };
 
 const addTime = () => {
   time += 1;
-  console.log(time);
+  secondsElement.innerHTML = Math.floor(time / 100);
+  milisecondsElement.innerHTML = Math.floor(time % 100);
 };
 
 startButton.addEventListener('click', startTime);
